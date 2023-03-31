542ac42a08c53299bc39147e136f21ad64de23eb
b-abdenoure <nrboxworking@gmail.com>
2023-03-31 19:04:55 +0100

    Initial Commit

---
+++ uiCollection View practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uiCollection View practice
+//
+//  Created by Abdenoure Boudlal on 3/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
