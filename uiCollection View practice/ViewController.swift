3839c3f95e44ffda5f508aab5e4a95cdef68b207
b-abdenoure <nrboxworking@gmail.com>
2023-04-01 17:38:19 +0100

    the needed result of displaying json data into a collectionView work great, i still have to explore more possibilities but the basics just work. in this exercice i tried to fetch only one category which is electronics from the fake store API. next step gonna be the merge of single product as a single view, a table view and a collection view with a separated networking command for one product, fun catalogue and a category.

--- uiCollection View practice/ViewController.swift
+++ uiCollection View practice/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var productsCollectionView: UICollectionView!
-    
+    var productsArrayCell = [Products]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        fetchProductsData { (products) in
+            self.productsArrayCell = products
+            DispatchQueue.main.async {
+                self.productsCollectionView.reloadData()
+            }
+            print(products)
+            for product in products{
+            }
+        }
+//        productsCollectionView.delegate = self
+//        productsCollectionView.dataSource = self
+//        productsCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
     }
 
     let productURL = "https://fakestoreapi.com/products/category/electronics"
@@ -33,14 +45,41 @@ class ViewController: UIViewController {
             }
             catch{
                 let error = error
-                print(error.localizedDescription)
-                print(String(describing: error))
+//                print(error.localizedDescription)
+//                print(String(describing: error))
 
             }
         }
-        
         .resume()
     }
 }
 
-
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return productsArrayCell.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        as! MyCollectionView
+        var product = productsArrayCell[indexPath.row]
+        
+        collectionCell.titleCell.text = product.title!
+        let price = "\(String(describing: product.price!))"
+        collectionCell.priceCell.text = price + " $"
+        collectionCell.descriptionCell.text = product.description!
+        DispatchQueue.main.async {
+            let url = URL(string: product.image)!
+            if let data = try? Data(contentsOf: url) {
+                collectionCell.imageCell.image = UIImage(data: data)
+            }
+        }
+        return collectionCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let size = (collectionView.frame.size.width ) / 0.8
+        return CGSize(width: 300, height: 300)
+    }
+    
+}
